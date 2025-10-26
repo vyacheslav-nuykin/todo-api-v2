9216815b38f477e3b52e752e5a83d088888f8ddc
Vyacheslav Nuykin <nujkinvaceslav@gmail.com>
2025-10-26 10:04:24 +0300

    Created DTO Response

--- src/main/java/com/vyacheslav/todo_api/dto/response/TodoResponse.java
+++ src/main/java/com/vyacheslav/todo_api/dto/response/TodoResponse.java
@@ -1,4 +1,24 @@
 package com.vyacheslav.todo_api.dto.response;
 
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 public class TodoResponse {
+
+    private Long id;
+    private String title;
+    private boolean completed = false;
+    private LocalDateTime createdAt;
+
+    public TodoResponse(Long id, String title, boolean completed, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.createdAt = createdAt;
+    }
 }
