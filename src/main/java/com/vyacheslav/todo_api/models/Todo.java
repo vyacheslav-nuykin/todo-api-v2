a29a660ee4ef6c83796d7efdd14506ace5e000e3
Vyacheslav Nuykin <nujkinvaceslav@gmail.com>
2025-10-26 09:54:18 +0300

    Rework Model(entity)

--- src/main/java/com/vyacheslav/todo_api/models/Todo.java
+++ src/main/java/com/vyacheslav/todo_api/models/Todo.java
@@ -1,30 +1,46 @@
 package com.vyacheslav.todo_api.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "todos")
+@Getter
+@NoArgsConstructor
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 2048)
     private String title;
+
     private boolean completed = false;
     private LocalDateTime createdAt;
-
-    public Todo() {}
 
     public Todo(String title, boolean completed) {
         this.title = title;
         this.completed = completed;
-        this.createdAt = LocalDateTime.now();
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public boolean isCompleted() {
+        return completed;
+    }
 
-    public Boolean isCompleted() { return completed; }
-    public void setCompleted(Boolean completed) { this.completed = completed; }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
